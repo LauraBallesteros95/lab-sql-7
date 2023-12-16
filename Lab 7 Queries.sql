cb155f2c5a467499ca49cad57e110012d838be29
LauraBallesteros95 <l.ballesteros.zafra@gmail.com>
2023-12-17 00:23:58 +0100

    :modified the file to push

---
+++ Lab 7 Queries.sql
@@ -0,0 +1,32 @@
+-- LAB QUERIES 7
+-- In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list.
+select last_name
+from actor
+group by last_name
+having count(*) = 1;
+-- Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors where the last name was present more than once
+select last_name
+from actor
+group by last_name
+having count(*) > 1;
+-- Using the rental table, find out how many rentals were processed by each employee.
+select staff_id, count(rental_id) as 'count'
+from rental
+group by staff_id;
+-- Using the film table, find out how many films were released each year.
+select release_year, count(film_id)
+from film 
+group by release_year;
+-- Using the film table, find out for each rating how many films were there.
+select rating, count(film_id) as 'Count Films'
+from film
+group by rating;
+-- What is the mean length of the film for each rating type. Round off the average lengths to two decimal places.
+select rating, round(avg(length),2) as 'Mean Length'
+from film
+group by rating;
+-- Which kind of movies (rating) have a mean duration of more than two hours?
+select avg(length) as ALength, rating
+from film
+group by rating
+having ALength > 120;
